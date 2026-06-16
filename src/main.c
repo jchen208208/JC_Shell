6ce4425fed05a17d451462cff84c52fc7cdf8214
sandmanfan11 <jaydenccan11@gmail.com>
2026-06-15 21:59:34 -0700

    Force fix for stage 1

--- src/main.c
+++ src/main.c
@@ -1,12 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) {
-  // Flush after every printf
-  setbuf(stdout, NULL);
-
-  // TODO: Uncomment the code below to pass the first stage
-  // printf("$ ");
-
-  return 0;
+int main() {
+    setbuf(stdout, NULL);
+    printf("$ ");
+    return 0;
 }

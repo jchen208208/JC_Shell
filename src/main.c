ecde776d34cb728c885684352e38f460237d17aa
sandmanfan11 <jaydenccan11@gmail.com>
2026-06-15 22:38:41 -0700

    fix

--- src/main.c
+++ src/main.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main() {
     setbuf(stdout, NULL);
-    printf("$ ");
+    while (1) {
+        printf("$ ");
+        char input[100];
+        if (fgets(input, 100, stdin) == NULL) break;
+        input[strcspn(input, "\n")] = '\0';
+        printf("%s: command not found\n", input);
+    }
     return 0;
 }

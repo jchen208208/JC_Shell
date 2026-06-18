cbd6f06970e60bb3b729fcca3945d5a337f40f7d
sandmanfan11 <jaydenccan11@gmail.com>
2026-06-17 21:39:11 -0700

    type builtin

--- src/main.c
+++ src/main.c
@@ -3,29 +3,46 @@
 #include <string.h>
 #include <stdbool.h>
 
+static bool is_builtin(const char *command) {
+    return strcmp(command, "echo") == 0 ||
+           strcmp(command, "exit") == 0 ||
+           strcmp(command, "type") == 0;
+}
+
 int main(int argc, char *argv[]) {
     setbuf(stdout, NULL);
-    
+
     while (true) {
-      printf("$ ");
+        printf("$ ");
 
-      // user input
-      char input[100];
-      fgets(input, 100, stdin);
-      input[strlen(input) - 1] = '\0';
-      
-      if (strcmp(input, "exit") == 0) {
-        break;
-      }
+        char input[100];
+        if (!fgets(input, sizeof(input), stdin)) {
+            break;
+        }
 
-      else if (strncmp(input, "echo ", 5) == 0) {
-        printf("%s\n", input + 5);
-      }
-      
-      else {
-        printf("%s: command not found\n", input);
-      }
+        input[strcspn(input, "\n")] = '\0';
+
+        if (strcmp(input, "exit") == 0) {
+            break;
+        }
+        
+        else if (strncmp(input, "echo ", 5) == 0) {
+            printf("%s\n", input + 5);
+        }
+        
+        else if (strncmp(input, "type ", 5) == 0) {
+            const char *command = input + 5;
+            if (is_builtin(command)) {
+                printf("%s is a shell builtin\n", command);
+            } else {
+                printf("%s: not found\n", command);
+            }
+        }
+        
+        else {
+            printf("%s: command not found\n", input);
+        }
     }
 
-  return 0;
+    return 0;
 }

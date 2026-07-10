3f3aa7e0b4a01724f20a1192fae6f9eb168c2a0c
sandmanfan11 <jaydenccan11@gmail.com>
2026-07-09 20:09:54 -0700

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <unistd.h>
 
 static bool is_builtin(const char *command) {
     return strcmp(command, "echo") == 0 ||
@@ -34,10 +35,36 @@ int main(int argc, char *argv[]) {
             const char *command = input + 5;
             if (is_builtin(command)) {
                 printf("%s is a shell builtin\n", command);
-            } else {
-                printf("%s: not found\n", command);
             }
-        }
+            else {
+                char *path_env = getenv("PATH");
+                int found = 0;
+
+                if (path_env != NULL) {
+                    // Duplicate path_env because strtok modifies the string
+                    char *path_copy = strdup(path_env);
+                    char *dir = strtok(path_copy, ":");
+
+                    while (dir != NULL) {
+                        char full_path[1024];
+                        snprintf(full_path, sizeof(full_path), "%s/%s", dir, command);
+
+                        // Check if file exists and is executable
+                        if (access(full_path, X_OK) == 0) {
+                            printf("%s is %s\n", command, full_path);
+                            found = 1;
+                            break;
+                        }
+                        dir = strtok(NULL, ":");
+                    }
+                    free(path_copy);
+                }
+
+                if (!found) {
+                    printf("%s: not found\n", command);
+                    }
+                }
+            }
         
         else {
             printf("%s: command not found\n", input);
